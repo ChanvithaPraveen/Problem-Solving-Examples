84558d591a87ce9521c4e3a7818b67c66712abb3
Chanvitha Praveen <chanvithapraween@gmail.com>
2024-04-27 10:49:22 +0530

    Created find last digit program

--- FindLastDigitOfNumber.cpp
+++ FindLastDigitOfNumber.cpp
@@ -1,32 +1,32 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//
-//    // 1st method
-//
-//    /*
-//    int number;
-//    int lastDigit;
-//
-//    cout << "Enter a number:";
-//    cin >> number;
-//    lastDigit = number%10;
-//    cout << "Last Digit: " << lastDigit;
-//    */
-//
-//    // 2nd method
-//
-//    int number;
-//    int lastDigit;
-//    string stringNumber;
-//
-//    cout << "Enter a number:";
-//    cin >> number;
-//    stringNumber = to_string(number);
-//    lastDigit = stringNumber[stringNumber.length()-1] - '0';
-//    cout << "Last Digit: " << lastDigit;
-//
-//
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // 1st method
+
+    /*
+    int number;
+    int lastDigit;
+
+    cout << "Enter a number:";
+    cin >> number;
+    lastDigit = number%10;
+    cout << "Last Digit: " << lastDigit;
+    */
+
+    // 2nd method
+
+    int number;
+    int lastDigit;
+    string stringNumber;
+
+    cout << "Enter a number:";
+    cin >> number;
+    stringNumber = to_string(number);
+    lastDigit = stringNumber[stringNumber.length()-1] - '0';
+    cout << "Last Digit: " << lastDigit;
+
+
+    return 0;
+}
